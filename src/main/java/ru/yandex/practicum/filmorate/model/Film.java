4d8979ebe8c8102f35c3bca8e667886c49072d75
Vladimir Novoseltsev <novoseltsev-v-vladimir@yandex.ru>
2025-08-13 22:33:17 +0300

    Исправить check-style v2

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,10 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.Duration;
 import java.time.LocalDate;
 
 /**
