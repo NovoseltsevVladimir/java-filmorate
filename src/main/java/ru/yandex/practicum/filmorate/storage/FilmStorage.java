b13553a324dc9ee663db5e90166f8051511f5481
NovoseltsevVladimir <novoseltsev-v-vladimir@yandex.ru>
2025-09-09 11:07:17 +0300

    Исправить check-style v4

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Rating;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface FilmStorage {
 
